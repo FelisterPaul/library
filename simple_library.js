6a6a2e2e18e032e439e7a361a73106e64062987b
stecy1 <felisterpaul4@gmail.com>
2023-07-28 08:41:51 +0300

    added books

--- simple_library.js
+++ simple_library.js
@@ -1,2 +1,41 @@
+//Declare books
+//Declare library Name
+
+// - number of books
+// - is borrowed
+// - current holder
+// - duration 
+// - borrowed date
+// - return date
+// - available books
+
 let libray_Name = ['Paul Library'];
-let books = ['George', '7 hours of sleep','Night Habits'];
+let books = ['Life of stories', '7 hours of sleep','Night Habits'];
+let book1 = [[12 'Life of a single mother'], 
+
+
+let books_ = {
+    book1: {  
+        name : 'life of stories',
+        pages : 125,
+        author : 'felister',
+        publisher : 'Syndey',
+    book2: {
+        name : '7 hours of sleep',
+        pages : 135,
+        author :'kelly',
+    book3 : {
+        name : 'Night habits',
+        pages : 145,
+        author : 'Dan',
+        publisher : 'Amie',
+    }
+
+    }
+
+
+    }
+             
+
+
+'}
